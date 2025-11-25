e12664361babaf9ae5b4b8409994df857b40a4a9
Mike Assis <mikheas.assis@gmail.com>
2025-11-24 21:37:25 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+print('Sabor Express\n')
+
+print('1. Listar restaurantes')
+print('2. Listar restaurantes')
+print('3. Listar restaurantes')
+print('4. Sair')
+
+opcao_escolhida = input('Escolha um opção: ')
+print('Você escolheu a opção escolhida', opcao_escolhida)
